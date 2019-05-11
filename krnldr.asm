73ffbd7f97c85525b17a92a2758062b5c802ad4a
USN484259 <48667071+USN484259@users.noreply.github.com>
2019-05-11 17:51:54 +0800

    Add files via upload

--- krnldr.asm
+++ krnldr.asm
@@ -3,37 +3,55 @@ IA32_EFER equ 0xC0000080
 
 
 TSS_BASE equ 0x1000
+TSS_LIM equ 0x200
 GDT_BASE equ 0x1200
 GDT_LIM equ 0x200	;64 entries
 IDT_BASE equ 0x1400
 IDT_LIM equ 0x400	;128 entries
 
-MEMSCAN_BASE equ 0x1800
-
-;MEMBMP_BASE equ 0x20000
-;MEMBMP_LIM equ 0x80000
+SYSINFO_BASE equ 0x1800
 
 
-PG4D equ 0x02000
+MEMSCAN_BASE equ 0x2000
 
 
+
+ISR_STK equ 0x04000
+;guard page at 0x02000
+GATE_STK equ 0x010000
+;guard page at 0x0C000
+
+PG4D equ 0x04000
+
+PT0 equ 0x0A000
+PDT0 equ 0x0B000
 ;	Memory layout
 ;	Virtual Address					Physical Address
 
 
-;	000000000000	000000001000							GAP	NULL address
-;									00000000	00001000	N	first PT for krnl
-;	000000001000	000000002000	00001000	00002000	R	TSS & GDT & IDT & systeminfo
-;	000000002000	000000010000	00002000	00010000	RW	PG & TSS stack
-;	800000010000	800000040000	00010000	00040000	RX	ldr code	avlPMM
-;	?								00040000	000A0000	?	avlPMM
-;									000A0000	00100000	N	BIOS area
-;	000000004000	000000010000	?						RW	common PMM map area
-;	?								00100000	MAXPMM		?	avlPMM
-;	000000010000	000080000000	(allocated)				?	user area
-;	000080000000	000100000000	(allocated)				RX	32bit krnl proxy
-;	000100000000	800000000000	(allocated)				?	user area
-;	800000000000   1000000000000	(allocated)				?	krnl area
+;	000000000000	000000001000							GAP		NULL address
+;									00000000	00001000	N		PDPT for krnl 512G layout fixed
+;	000000001000	000000002000	00001000	00002000	R(W)	TSS & GDT & IDT & systeminfo
+;	000000002000	000000004000	00002000	00004000	RW		ISR stack & PMMSCAN
+
+;	000000004000	000000005000	00004000	00005000	RW		PG4D
+;	000000005000	000000006000	00005000	00006000	RW		PDPT for krnl at high
+;	000000006000	000000007000	00006000	00007000	RW		PDT for krnl at 2G
+;	000000007000	000000008000	00007000	00008000	RW		PDT for krnl at 3G
+;	000000008000	00000000A000	00008000	0000A000	RW		PT for video memory
+;	00000000A000	00000000B000	0000A000	0000B000	RW		first PT for krnl
+;	00000000B000	00000000C000							GAP		guard page
+;									0000B000	0000C000	N		first PDT for krnl	1G layout fixed reused at high
+;	00000000C000	000000010000	0000C000	00010000	RW		GATE stack
+;	800000010000	800000020000	00010000	00020000	X		ldr code	avlPMM
+;	?								00020000	000A0000	?		avlPMM
+;									000A0000	00100000	N		BIOS area
+;	000000004000	000000010000	?						RW		common PMM map area
+;	?								00100000	MAXPMM		?		avlPMM
+;	000000010000	000080000000	(allocated)				?		user area
+;	000080000000	000100000000	(allocated)				RX		32bit krnl proxy
+;	000100000000	800000000000	(allocated)				?		user area
+;	800000000000   1000000000000	(allocated)				?		krnl area
 
 
 
@@ -45,26 +63,22 @@ section code vstart=0x10000
 
 jmp entry
 
+
+struc sysinfo
+.sig	resq 1
+.cpu	resd 1
+.bus	resw 1
+.port	resw 7
+.VBE_width	resw 1
+.VBE_height	resw 1
+.VBE_bpp	resw 1
+.VBE_mode	resw 1
+.VBE_addr	resd 1
+.VBE_lim	resd 1
+
+endstruc
+
 align 4
-
-dd 'INFO'
-cpuinfo dd 0
-addrinfo:
-db 36
-db 32
-dw 0
-
-VBE_width dw 0
-VBE_height dw 0
-VBE_bpp dw 24
-VBE_mode dw 0
-VBE_addr dd 0
-VBE_lim dd 0
-
-
-;env here
-
-align 16
 
 strboot db 'COFUOS Loading ...',0
 align 4
@@ -92,7 +106,7 @@ db 0		;sys ds
 
 dq 0	;end of GDT
 
-gdt32_len equ ($-gdt32_base-1)
+gdt32_len equ ($-gdt32_base)
 
 gdt64_base:
 dd 0
@@ -107,6 +121,13 @@ dd 0000_0000_0010_0000_1111_1100_0000_0000_b	;user CS	confirming ?
 dd 0
 dd 1111_0010_0000_0000_b	;user DS
 
+dw TSS_LIM
+dw TSS_BASE
+dw 0x8900
+dw 0
+dq 0		;TSS
+
+gdt64_len equ ($-gdt64_base)
 
 align 16
 
@@ -171,6 +192,30 @@ mov sp,0x7c00
 
 mov si,strboot
 call print16
+
+xor ax,ax
+mov di,SYSINFO_BASE
+mov cx,0x800/2
+rep stosw
+
+mov di,SYSINFO_BASE
+
+mov eax,'SYSI'
+stosd
+mov eax,'NFO'
+stosd
+
+add di,4
+mov ax,0x2024
+stosw
+
+add di,6
+mov ax,24
+stosw
+
+
+
+
 
 ;detect VBE
 mov di,MEMSCAN_BASE+0x200
@@ -242,19 +287,19 @@ mov es,cx
 
 
 
-cmp ax,[es:VBE_width]
+cmp ax,[es:SYSINFO_BASE+sysinfo.VBE_width]
 jb video_loop
-cmp dx,[es:VBE_height]
+cmp dx,[es:SYSINFO_BASE+sysinfo.VBE_height]
 jb video_loop
 
 movzx cx,BYTE [ss:MEMSCAN_BASE+0x19]
-cmp cx,[es:VBE_bpp]
+cmp cx,[es:SYSINFO_BASE+sysinfo.VBE_bpp]
 jb video_loop
 
 
 ;save current video mode
 
-mov di,VBE_width
+mov di,SYSINFO_BASE+sysinfo.VBE_width
 
 stosw
 mov ax,dx
@@ -285,7 +330,7 @@ mov es,dx
 mov si,strvberr
 
 ;TODO check if resolution is set
-mov bx,[VBE_mode]
+mov bx,[SYSINFO_BASE+sysinfo.VBE_mode]
 cmp dx,bx
 jz abort16
 mov ax,0x4F02
@@ -345,7 +390,7 @@ inc ax
 push ax
 mov ax,gdt32_base
 push ax
-mov ax,gdt32_len
+mov ax,gdt32_len-1
 push ax
 mov di,sp
 lgdt [ss:di]
@@ -367,9 +412,14 @@ align 4
 
 [bits 32]
 
+abort:
+hlt
+jmp abort
 
 
 zeromemory:		;edi target		ecx size align to DWORD
+push edi
+
 test cl,0011_b
 jnz abort
 xor eax,eax
@@ -377,19 +427,8 @@ shr ecx,2
 cld
 rep stosd
 
+pop edi
 ret
-
-
-abort:
-hlt
-jmp abort
-
-align 4
-
-strhello db 'HELLO',0
-strworld db 'WORLD',0
-strabort db 'ABORTED',0
-align 4
 
 
 pe_entry:
@@ -408,7 +447,7 @@ mov ebp,esp		;stack frame
 push 10_0000_0000_0000_0000_0010_b
 popfd
 
-
+nop
 
 ;check cpuid
 pushfd
@@ -431,7 +470,7 @@ xor ecx,ecx
 mov al,7
 cpuid
 
-mov [cpuinfo],ebx		;SMAP SMEP INVPCID	TODO
+mov [SYSINFO_BASE+sysinfo.cpu],ebx		;SMAP SMEP INVPCID	TODO
 
 cpuid0_basic:
 
@@ -463,7 +502,7 @@ jb cpuid8_basic
 mov al,8
 cpuid
 
-mov [addrinfo],ax	;MAXPHYADDR
+mov [SYSINFO_BASE+sysinfo.bus],ax	;MAXPHYADDR
 
 
 cpuid8_basic:
@@ -490,6 +529,13 @@ or ax,0000_1001_0000_0001_b		;NXE LME SCE
 wrmsr
 
 
+;save ports
+mov esi,0x0400
+mov edi,SYSINFO_BASE+sysinfo.port
+mov ecx,7
+rep movsw
+
+
 ;init GDT64
 mov edi,GDT_BASE
 mov ecx,GDT_LIM
@@ -497,47 +543,109 @@ call zeromemory
 
 mov esi,gdt64_base
 mov edi,GDT_BASE+8
-mov ecx,8
+mov ecx,gdt64_len/4
 rep movsd
 
+;init TSS
+mov edi,TSS_BASE
+mov ecx,TSS_LIM
+call zeromemory
+mov eax,GATE_STK
+add edi,4
+stosd
+mov eax,ISR_STK
+add edi,0x20
+stosd
 
 
 ;paging init
-;NOTE shall init PMMBMP first
 
-cld
+xor edi,edi
+mov ecx,0x1000
+call zeromemory
+mov eax,PDT0 | 3
+stosd
+
+
+mov edi,PG4D
+mov ecx,(0xC000-PG4D)
+call zeromemory
+
 xor ecx,ecx
 
-
-mov edx,PG4D
-mov edi,edx
+mov edx,edi
+mov cl,3
 mov cr3,edx
-add edx,0x1000
-mov eax,edx
-mov al,7
-stosd
 mov [edi],ecx
 
-mov edi,edx
+
+
 add edx,0x1000
+add edi,0x0800
 mov eax,edx
-mov al,7
+
+mov al,3
 stosd
-mov [edi],ecx
 
 mov edi,edx
+mov eax,PDT0 | 3
+stosd
+
+;TODO set video memory here
+
+
+
+mov edx,PT0
+
+mov edi,PDT0
+mov eax,edx
+mov al,3
+stosd
+
+;set pages of first 2MB
 xor eax,eax
-mov al,0x83		;set PAT here
+mov edi,edx	;PT0
 stosd
-mov [edi],ecx
+stosd
+
+mov ax,0x1003
+mov edx,0x80000000
+
+mov ecx,0x0F
+
+PT0_loop1:
+stosd
+add eax,0x1000
+mov [edi],edx
+add edi,4
+
+loop PT0_loop1
+
+mov ebx,edi
+
+btc eax,1
+mov ecx,0x10
+
+PT0_loop2:
+
+stosd
+add eax,0x1000
+add edi,4
+
+loop PT0_loop2
+
+;clear guard page
+xor eax,eax
+sub ebx,(4+1)*8
+stosd
+
 
 ;enable PG
 mov eax,cr0
 bts eax,31
 mov cr0,eax
 
-;NOTE GDT and TSS no longer write-able since now
-;All 'data' values in this ldr shall have been set
+;NOTE GDT IDT TSS and SYSINFO no longer write-able since now
 
 ;test LMA
 
@@ -566,16 +674,37 @@ align 16
 
 LM_entry:
 
+
 mov ax,0x10
 mov ds,ax
 mov es,ax
 mov fs,ax
 mov gs,ax
 mov ss,ax
-mov rsp,0x10000
+mov rsp,ldrstk
 mov rbp,rsp
 
+mov rdx,LM_high
 
+jmp rdx
+
+align 16
+
+CODE64OFF equ ($-$$)
+
+section codehigh vstart=0xFFFF8000_00010000+CODE64OFF
+
+ldrstk:
+
+TR_selector:
+dw 5*8
+
+align 8
+
+LM_high:
+
+mov rax,TR_selector
+ltr [rax]
 
 
 
